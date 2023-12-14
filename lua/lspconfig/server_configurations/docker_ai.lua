2367493ed82f0edd51676c63d27f9f7e7854cad5
Jim Clark <slimslenderslacks@gmail.com>
2023-12-13 19:34:18 -0800

    add docker_ai module

--- lua/lspconfig/server_configurations/docker_ai.lua
+++ lua/lspconfig/server_configurations/docker_ai.lua
@@ -3,17 +3,18 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { '/Users/slim/atomisthq/lsp/start.sh' },
-    filetypes = { 'dockerfile', 'clojure' },
+    filetypes = { },
     root_dir = util.root_pattern('Dockerfile'),
   },
   docs = {
     description = [[
-https://github.com/docker/lsp
+https://github.com/docker/labs-assistant-ml
 
-Docker Language Server
+Docker AI Language Server
 ]],
     default_config = {
       root_dir = [[root_pattern("Dockerfile")]],
     },
   },
 }
+
